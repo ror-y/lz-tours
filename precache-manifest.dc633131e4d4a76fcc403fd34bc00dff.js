c4969850035132d7137b731244f61fa5b9c73dbc
Rory Smith <rorysmith@Rorys-MacBook-Pro.local>
2020-04-12 16:44:21 +0100

    Updates

---
+++ precache-manifest.dc633131e4d4a76fcc403fd34bc00dff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dec3bd731018adfeeecf401277a4c47",
+    "url": "/lz-tours/index.html"
+  },
+  {
+    "revision": "64a754d0c9df7bace81b",
+    "url": "/lz-tours/static/css/main.e90ff1b8.chunk.css"
+  },
+  {
+    "revision": "62dc4522c8ebc17463b8",
+    "url": "/lz-tours/static/js/2.e38e4698.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/lz-tours/static/js/2.e38e4698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a754d0c9df7bace81b",
+    "url": "/lz-tours/static/js/main.c13775a1.chunk.js"
+  },
+  {
+    "revision": "f1610bf8681422358dd7",
+    "url": "/lz-tours/static/js/runtime-main.8372b63b.js"
+  }
+]);
